b7f371d8d937fd46124f9eb75fef6e25c20f87a2
simonrowlands <si_rhys96@hotmail.co.uk>
2019-02-27 17:15:08 +0000

    Removed an align func, should use pin here Changed align variable to be more readable

--- novoda-constraints/Classes/UIView+AlignToView.swift
+++ novoda-constraints/Classes/UIView+AlignToView.swift
@@ -2,26 +2,12 @@ import UIKit
 
 public extension UIView {
     @discardableResult func align(_ edge: Edge,
-                                  to view: UIView,
+                                  with view: UIView,
                                   constant: CGFloat = 0,
                                   priority: UILayoutPriority = .required,
                                   relatedBy relation: NSLayoutRelation = .equal) -> NSLayoutConstraint {
         return pin(edge,
                    to: edge,
-                   of: view,
-                   constant: constant,
-                   priority: priority,
-                   relatedBy: relation)
-    }
-    
-    @discardableResult func align(_ edge: Edge,
-                                  to otherEdge: Edge,
-                                  of view: UIView,
-                                  constant: CGFloat = 0,
-                                  priority: UILayoutPriority = .required,
-                                  relatedBy relation: NSLayoutRelation = .equal) -> NSLayoutConstraint {
-        return pin(edge,
-                   to: otherEdge,
                    of: view,
                    constant: constant,
                    priority: priority,
