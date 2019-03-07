6b62b8ed074df69d97b84eebc3ed72928467ab3f
Simon Rowlands <simon.rowlands@novoda.com>
2019-03-07 17:54:39 +0000

    Changed the align func to allow for multiple edges

--- novoda-constraints/Classes/UIView+AlignToView.swift
+++ novoda-constraints/Classes/UIView+AlignToView.swift
@@ -1,16 +1,32 @@
 import UIKit
 
 public extension UIView {
-    @discardableResult func align(_ edge: Edge,
-                                  with view: UIView,
-                                  constant: CGFloat = 0,
-                                  priority: UILayoutPriority = .required,
-                                  relatedBy relation: NSLayoutConstraint.Relation = .equal) -> NSLayoutConstraint {
-        return pin(edge,
-                   to: edge,
-                   of: view,
-                   constant: constant,
-                   priority: priority,
-                   relatedBy: relation)
+    func align(_ edges: Edge...,
+               with view: UIView,
+               constant: CGFloat = 0,
+               priority: UILayoutPriority = .required,
+               relatedBy relation: NSLayoutConstraint.Relation = .equal) {
+        
+        align(edges,
+              with: view,
+              constant: constant,
+              priority: priority,
+              relatedBy: relation)
+    }
+    
+    func align(_ edges: [Edge],
+               with view: UIView,
+               constant: CGFloat = 0,
+               priority: UILayoutPriority = .required,
+               relatedBy relation: NSLayoutConstraint.Relation = .equal) {
+        
+        for edge in edges {
+            pin(edge,
+                to: edge,
+                of: view,
+                constant: constant,
+                priority: priority,
+                relatedBy: relation)
+        }
     }
 }
