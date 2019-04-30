8d7e995f6f0c557177346c9a0043bbdd4e13a012
simonrowlands <simon.rowlands@novoda.com>
2019-04-30 10:56:39 +0100

    Updating file names + cleanup

--- novoda-constraints/Classes/Edge.swift
+++ novoda-constraints/Classes/Edge.swift
@@ -6,11 +6,8 @@ public struct Edge: Equatable {
     let insetMultiplier: CGFloat
     
     public static let top = Edge(layoutAttribute: .top, insetMultiplier: 1)
-
     public static let bottom = Edge(layoutAttribute: .bottom, insetMultiplier: -1)
-
     public static let leading = Edge(layoutAttribute: .leading, insetMultiplier: 1)
-
     public static let trailing = Edge(layoutAttribute: .trailing, insetMultiplier: -1)
     
     public static let all: [Edge] = [.top, .bottom, .leading, .trailing]
